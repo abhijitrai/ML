f483cc492f079cc7def69acfe8c7e97317d356fa
Abhijt Rai <abhijit.ddn@gmail.com>
2021-04-28 22:24:39 +0530

    updateTheta

--- MIT/6036/week1/Perceptron_Through_Origin.py
+++ MIT/6036/week1/Perceptron_Through_Origin.py
@@ -4,14 +4,13 @@ from Tools import distanceFromPlane as dist
 from solution.solution_data import labels1, data1, data2, labels2
 from solution.solution_plot import plot_data, plot_separator
 
-
-def calcEqVal(X, Th, Th0):
-    thTranspose = np.transpose(np.array(Th))
-    vector = np.array(X)
-    dotProduct = np.dot(thTranspose, vector)
-    # dotProduct = np.sum(np.array(Th)  *  vector)
-    res = dotProduct + Th0
-    return np.asscalar(res)
+#
+# def calcEqVal(X, Th, Th0):
+#     thTranspose = np.transpose(np.array(Th))
+#     vector = np.array(X)
+#     dotProduct = np.dot(thTranspose, vector)
+#     res = dotProduct + Th0
+#     return np.asscalar(res)
 
 
 def updateTheta(th, testPoint, predictedVal):
@@ -33,19 +32,39 @@ def testRun(sampleData, th):
     print('***************************Test Run End *************************************')
 
 
-def perceptron_test(th, x, y):
+def perceptron_th_check(th, x, y):
+    """
+    Returns the value of Eq : Y(i) . ( Th.T X(i) )
+    Here
+    1. ( Th.T X(i) ) determines in which half space the point lie ( +ve /-ve ) .
+    This is our prediction as per the Th
+    2. Y(i) is the actual label ( +1 / -1 )
+    Hence if the sign as determined by Th is equal to sign if the label , we get a +ve val back.
+    Else we get a -ve value back
+    :param th: Theta is a Vector of n dimensions
+    :param x:  Point is a vector of n dimensions
+    :param y:  Label is the true label
+    :return:   Computed signed value
+    """
     val = y * (np.dot(np.transpose(th), x))
     return val
 
 
 def algo(sample_data, th, T=20, plotIntermediate=True):
+    """
+    Tries to finf the Linear Seperator by the Perceptron method
+    :param sample_data:
+    :param th: Th init to 0 vector
+    :param T: Iterations to run
+    :param plotIntermediate:  Plot intermediate results
+    """
     count = 0
     for i in range(T):
         changes = 0
         for data in sample_data:
             testPoint = np.array(cv(data[0]))
             predictedVal = data[1]
-            if (perceptron_test(th, testPoint, predictedVal) <= 0):
+            if (perceptron_th_check(th, testPoint, predictedVal) <= 0):
                 print('Sample Run with Th -> ', th.flatten())
                 testRun(sample_data, th)
                 th = updateTheta(th, testPoint, predictedVal)
@@ -70,10 +89,6 @@ def algo(sample_data, th, T=20, plotIntermediate=True):
     testRun(sample_data, th)
 
 
-#
-# X, Y = super_simple_separable_through_origin()
-# print(X)
-# print(Y)
 
 def formatInputData(data, labels):
     xVals = data[0]
